48afe983ef078ef796aac45c2c5ef0a0353bea69
Ruxue <1078134231@qq.com>
2020-10-31 15:30:27 +0800

    修改路由

---
+++ src/views/acount/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    登录页面
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login"
+});
+</script>
